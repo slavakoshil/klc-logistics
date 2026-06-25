7b43c4d226ebc205d169e260036eba32d2530573
slavakoshil <slava.koshil@gmail.com>
2026-06-25 13:13:11 +0200

    Create KlcBackendApplication.java

---
+++ backend/src/main/java/com/klc/KlcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.klc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlcBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlcBackendApplication.class, args);
+    }
+
+}
